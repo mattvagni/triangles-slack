376fc183c33a33681f364a9b8b079a318bcc64d8
Mathias Vagni <mathiasvagni@googlemail.com>
2018-06-19 20:44:51 +0100

    Add debug logging to lambda function

--- src/lambda/health-check.js
+++ src/lambda/health-check.js
@@ -1,6 +1,12 @@
+import { parse } from 'querystring';
+
 export function handler(event, context, callback) {
+  const parsedBody = parse(event.body);
+  console.log(event);
+  console.log(parsedBody);
+
   callback(null, {
     statusCode: 200,
-    body: 'ok',
+    body: `Hey! It's all good.`,
   });
 }
