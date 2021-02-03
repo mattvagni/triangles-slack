c068adf3f7a2b0cf905afeebe6be8b0889757f82
Mathias Vagni <mattvagni@users.noreply.github.com>
2021-02-03 18:34:03 +0000

    Update index.js

--- src/web/JoinForm/index.js
+++ src/web/JoinForm/index.js
@@ -140,7 +140,7 @@ class JoinForm extends React.Component {
             autoComplete="off"
             required={true}
             type="text"
-            pattern="^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$"
+            pattern="(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
             title="Must be a valid URL."
             value={this.state.link}
             onChange={this.onFieldChange}
